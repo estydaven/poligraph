b3433e77cf51d0cde633007d7deebdaedd64b601
estydaven <esetnod1111@gmail.com>
2022-08-09 17:50:46 +0300

    feat: add mobile styles

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $(document).ready(function(){
         slidesToScroll: 1,
         prevArrow: '<div class="slider__arrow slider__arrow_prev"></div>',
         nextArrow: '<div class="slider__arrow slider__arrow_next"></div>',
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            }
+          ]
     });
 });
