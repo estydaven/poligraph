183a0f040e88d792f724d45b30f79d64ce5e148a
estydaven <esetnod1111@gmail.com>
2022-08-10 11:15:13 +0300

    feat: add timer script and some little fixes in mob

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Slider script
+
 $(document).ready(function(){
     $('.slider').slick({
         infinite: true,
@@ -17,3 +19,45 @@ $(document).ready(function(){
           ]
     });
 });
+
+// Timer script
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+ 
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+ 
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+ 
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+ 
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      var deadline = new Date(Date.parse(new Date()) + 5 * 60 * 60 * 1000);
+      initializeClock('countdown', deadline);
+    }
+  }
+ 
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+ 
+var deadline = new Date(Date.parse(new Date()) + 5 * 60 * 60 * 1000);
+initializeClock('countdown', deadline);
